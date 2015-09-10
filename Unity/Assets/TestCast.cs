3fa5ff8a833393f480f122167667c5758b694958
Graham Reeves <graham@grahamreeves.com>
2015-09-10 22:53:59 +0100

    Added an auto-connect caster script in the client

---
+++ Unity/Assets/TestCast.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCast : MonoBehaviour {
+
+	public float	Wait = 2;
+	public PopCast	mCast = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (mCast != null) {
+			mCast.Update();
+			return;
+		}
+
+		Wait -= Time.deltaTime;
+		if (Wait > 0)
+			return;
+
+		mCast = new PopCast ("chromecast:*", new PopCastParams ());
+
+
+	}
+}
