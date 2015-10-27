78c7066e0d3b323d28d40098a3aa52b7bb03f55f
Graham Reeves <graham@grahamreeves.com>
2015-10-27 17:54:07 +0000

    Added stub of H264 atom multiplexing (from Bento4)

--- src/TFileCaster.cpp
+++ src/TFileCaster.cpp
@@ -16,9 +16,13 @@ TFileCaster::TFileCaster(const TCasterParams& Params) :
 	mFrameBuffer.reset( new TMediaPacketBuffer() );
 	this->WakeOnEvent( mFrameBuffer->mOnNewPacket );
 	
-	//	alloc encoder
+	//	alloc encoder per stream
 	size_t StreamIndex = 0;
 	mEncoder.reset( new Avf::TEncoder( Params, mFrameBuffer, StreamIndex ) );
+	
+	//	alloc muxer
+	mMulitplexer.reset( new TMultiplexerMp4( mFileData ) );
+	this->WakeOnEvent( mFileData.mOnDataPushed );
 	
 	Start();
 }
@@ -36,28 +40,52 @@ TFileCaster::~TFileCaster()
 
 bool TFileCaster::Iteration()
 {
-	try
+	//	push frames to muxer
+	while ( true )
 	{
-		//	pop frames
-		auto pPacket = mFrameBuffer->PopPacket();
-		if ( !pPacket )
-			return true;
+		try
+		{
+			//	pop frames
+			auto pPacket = mFrameBuffer->PopPacket();
+			if ( !pPacket )
+				break;
 
-		auto& Packet = *pPacket;
-	
-		//	write frame
-		auto* Data = reinterpret_cast<const char*>( Packet.mData.GetArray() );
-		size_t DataSize = Packet.mData.GetDataSize();
-		mFileStream.write( Data, DataSize );
-	
-		//Soy::Assert( File.fail(), "Error writing to file" );
-		Soy::Assert( !mFileStream.bad(), "Error writing to file" );
+			auto& Packet = *pPacket;
+			mMulitplexer->Push( Packet );
+		
+		}
+		catch (std::exception& e)
+		{
+			std::Debug << "TFileCaster get-packet error: " << e.what() << std::endl;
+			break;
+		}
 	}
-	catch (std::exception& e)
+	
+	//	write to file
+	while ( mFileData.GetBufferedSize() > 0 )
 	{
-		std::Debug << "TFileCaster error: " << e.what() << std::endl;
+		try
+		{
+			Array<char> Buffer;
+			if ( !mFileData.Pop( mFileData.GetBufferedSize(), GetArrayBridge(Buffer) ) )
+				break;
+			
+			if ( Buffer.IsEmpty() )
+				break;
+			
+			//	write frame
+			mFileStream.write( Buffer.GetArray(), Buffer.GetDataSize() );
+			
+			//Soy::Assert( File.fail(), "Error writing to file" );
+			Soy::Assert( !mFileStream.bad(), "Error writing to file" );
+		}
+		catch (std::exception& e)
+		{
+			std::Debug << "TFileCaster write-file error: " << e.what() << std::endl;
+			break;
+		}
 	}
-	
+
 	return true;
 }
 
@@ -82,6 +110,187 @@ void TFileCaster::Write(const std::shared_ptr<SoyPixelsImpl> Image,SoyTime Timec
 	mEncoder->Write( Image, Timecode );
 }
 
+/*
+const uint32 AP4_FTYP_BRAND_ISOM = AP4_ATOM_TYPE('i','s','o','m');
+const AP4_UI32 AP4_FTYP_BRAND_MP41 = AP4_ATOM_TYPE('m','p','4','1');
+const AP4_UI32 AP4_FTYP_BRAND_MP42 = AP4_ATOM_TYPE('m','p','4','2');
+const AP4_UI32 AP4_FTYP_BRAND_CCFF = AP4_ATOM_TYPE('c','c','f','f');
+const AP4_UI32 AP4_FTYP_BRAND_ISML = AP4_ATOM_TYPE('i','s','m','l');
+const AP4_UI32 AP4_FTYP_BRAND_PIFF = AP4_ATOM_TYPE('p','i','f','f');
+const AP4_UI32 AP4_FTYP_BRAND_ISO2 = AP4_ATOM_TYPE('i','s','o','2');
+const AP4_UI32 AP4_FTYP_BRAND_MSDH = AP4_ATOM_TYPE('m','s','d','h');
+*/
+TMultiplexerMp4::TMultiplexerMp4(TStreamBuffer& Output) :
+	mOutput		( Output )
+{
+	//	versions from here
+	//	https://github.com/EyeSee360/Bento4/blob/master/Source/C%2B%2B/Apps/Mp4Mux/Mp4Mux.cpp
+/*
+	mAtom.mBrand = AP4_FILE_BRAND_MP42;
+	mAtom.mBrandVersion = 1;
+	mAtom.mCompatbileBrands.PushBack( AP4_FILE_BRAND_ISOM );
+	mAtom.mCompatbileBrands.PushBack( AP4_FILE_BRAND_MP42 );
+ */
+}
+	
+void TMultiplexerMp4::Push(TMediaPacket& Packet)
+{
+	//	push packet into movie
+}
+
+/*
+void SoyMediaAtom_FTYP::Write(TStreamBuffer& Data)
+{
+	SoyMediaAtom::Write( Data );
+	
+	Array<char> FieldData;
+	FieldData.PushBackReinterpret( mBrand );
+	FieldData.PushBackReinterpret( mBrandVersion );
+	for ( int i=0;	i<mCompatbileBrands.GetSize();	i++ )
+		FieldData.PushBackReinterpret( mCompatbileBrands[i] );
+	Data.Push( GetArrayBridge(FieldData) );
+}
+
+void TMultiplexerMp4::WriteHeader()
+{
+	//	write file atom
+	mFileAtom.Write( mOutput );
+	
+	//	write other atoms except for ftyp, moov and mdat
+	for ( int a=0;	a<mSubAtoms.GetSize();	a++ )
+	{
+		auto& Atom = mSubAtoms.GetSize();
+		auto AtomType = Atom.GetType();
+		if ( AtomType == AP4_ATOM_TYPE_MDAT ||
+			AtomType == AP4_ATOM_TYPE_FTYP ||
+			AtomType == AP4_ATOM_TYPE_MOOV )
+		{
+			continue;
+		}
+	}
+	
+	for ( int t=0;	t<mTracks.GetSize();	t++ )
+		mTracks[t].RecalcChunkOffsets();
+	
+	mMoovAtom.Write( Data );
+	
+	mMdatAtom.Write( Data );
+
+	for ( int t=0;	t<mTracks.GetSize();	t++ )
+	{
+		auto& Track = mTracks[t];
+
+		AP4_Track*    track = track_item->GetData();
+		AP4_TrakAtom* trak  = track->UseTrakAtom();
+		
+		// restore the backed-up chunk offsets
+		result = trak->SetChunkOffsets(*trak_chunk_offsets_backup[t]);
+		
+		// write all the track's samples
+		AP4_Cardinal   sample_count = track->GetSampleCount();
+		AP4_Sample     sample;
+		AP4_DataBuffer sample_data;
+		for (AP4_Ordinal i=0; i<sample_count; i++) {
+			track->ReadSample(i, sample, sample_data);
+			stream.Write(sample_data.GetData(), sample_data.GetDataSize());
+		}
+	}
+	
+
+}
+
+void SoyMediaAtom::Write(TStreamBuffer& Data)
+{
+	//	write header
+	{
+		Array<char> Header;
+		uint32 Size32;
+		uint64 Size64;
+		uint32 Type = x;
+		GetHeaderSize( SIze32, Size64 );
+		Header.PushBackReinterpret( Size32 );
+		Header.PushBackReinterpret( Type );
+		if ( Size32 == 1 )
+			Header.PushBackReinterpret( Size64 );
+
+		// for full atoms, write version and flags
+		if ( IsFullAtom() )
+		{
+			result = stream.WriteUI08(m_Version);
+			if (AP4_FAILED(result)) return result;
+			result = stream.WriteUI24(m_Flags);
+			if (AP4_FAILED(result)) return result;
+		}
+		
+		Data.Push( GetArrayBridge(Header) );
+	}
+}
 
 
-
+void SoyMediaAtom::Write(TStreamBuffer& Data)
+{
+	//	write header
+	{
+		Array<char> Header;
+		uint32 Size32;
+		uint64 Size64;
+		uint32 Type = x;
+		GetHeaderSize( SIze32, Size64 );
+		Header.PushBackReinterpret( Size32 );
+		Header.PushBackReinterpret( Type );
+		if ( Size32 == 1 )
+			Header.PushBackReinterpret( Size64 );
+		
+		// for full atoms, write version and flags
+		if ( IsFullAtom() )
+		{
+			result = stream.WriteUI08(m_Version);
+			if (AP4_FAILED(result)) return result;
+			result = stream.WriteUI24(m_Flags);
+			if (AP4_FAILED(result)) return result;
+		}
+		
+		Data.Push( GetArrayBridge(Header) );
+	}
+	
+	{
+		Array<char> FieldData;
+		FieldData.PushBackReinterpret( mBrand );
+		FieldData.PushBackReinterpret( mBrandVersion );
+		for ( int i=0;	i<mCompatbileBrands.GetSize();	i++ )
+			FieldData.PushBackReinterpret( mCompatbileBrands[i] );
+		Data.Push( GetArrayBridge(FieldData) );
+	}
+	
+	for ( int t=0;	t<mTracks.GetSize();	t++ )
+		mTracks[t].RecalcChunkOffsets();
+	
+	mMoovAtom.Write( Data );
+	
+	// create and write the media data (mdat)
+	// FIXME: this only supports 32-bit mdat size
+	stream.WriteUI32((AP4_UI32)mdat_size);
+	stream.WriteUI32(AP4_ATOM_TYPE_MDAT);
+	
+	// write all tracks and restore the chunk offsets to their backed-up values
+	for (AP4_List<AP4_Track>::Item* track_item = movie->GetTracks().FirstItem();
+		 track_item;
+		 track_item = track_item->GetNext(), ++t) {
+		AP4_Track*    track = track_item->GetData();
+		AP4_TrakAtom* trak  = track->UseTrakAtom();
+		
+		// restore the backed-up chunk offsets
+		result = trak->SetChunkOffsets(*trak_chunk_offsets_backup[t]);
+		
+		// write all the track's samples
+		AP4_Cardinal   sample_count = track->GetSampleCount();
+		AP4_Sample     sample;
+		AP4_DataBuffer sample_data;
+		for (AP4_Ordinal i=0; i<sample_count; i++) {
+			track->ReadSample(i, sample, sample_data);
+			stream.Write(sample_data.GetData(), sample_data.GetDataSize());
+		}
+	}
+	
+}
+*/
--- src/TFileCaster.h
+++ src/TFileCaster.h
@@ -2,10 +2,92 @@
 
 #include "TCaster.h"
 #include <fstream>
+#include <SoyStream.h>
 
 class TMediaPacket;
 class TMediaEncoder;
 class TMediaPacketBuffer;
+
+
+/*
+class SoyMediaAtom
+{
+public:
+	virtual void	Write(TStreamBuffer& Data);
+	virtual void	WriteFields(TStreamBuffer& Data);
+	
+protected:
+	virtual bool	IsFullAtom()	{	return false;	}
+	virtual uint32	GetType()=0;
+	virtual size_t	GetHeaderSize()	{	return AP4_ATOM_HEADER_SIZE;	}
+
+private:
+	virtual void	GetHeaderSize(uint32& Size32,uint64& Size64);
+	
+public:
+};
+
+
+
+class SoyMediaAtom_FTYP : public SoyMediaAtom
+{
+public:
+	virtual void	Write(TStreamBuffer& Data);
+
+protected:
+	virtual bool	IsFullAtom()		{	return false;	}
+	virtual uint32	GetType() override	{	return AP4_ATOM_TYPE_FTYP;	}
+	virtual size_t	GetHeaderSize() override	{	return SoyMediaAtom::GetHeaderSize() + XX + compatbielsize;		}
+	
+public:
+	uint32			mBrand;
+	uint32			mBrandVersion;
+	Array<uint32>	mCompatbileBrands;
+};
+
+
+class SoyMediaAtom_MOOV : public SoyMediaAtom
+{
+protected:
+};
+
+
+class SoyMediaAtom_MDAT : public SoyMediaAtom
+{
+protected:
+	virtual bool	IsFullAtom()		{	return false;	}
+	virtual uint32	GetType() override	{	return AP4_ATOM_TYPE_MDAT;	}
+	virtual size_t	GetHeaderSize() override	{	return mdat_size;		}
+};
+
+
+class TMultiplexerTrack
+{
+public:
+	void		RecalcChunkOffsets();
+};
+ */
+
+class TMultiplexerMp4
+{
+public:
+	TMultiplexerMp4(TStreamBuffer& Output);
+	
+	void			Push(TMediaPacket& Packet);
+	
+	void			WriteHeader();
+	
+public:
+ /*
+	SoyMediaAtom_FTYP			mFileAtom;
+	SoyMediaAtom_MOOV			mMoovAtom;
+	Array<SoyMediaAtom>			mSubAtoms;
+	Array<TMultiplexerTrack>	mTracks;
+	*/
+	TStreamBuffer&				mOutput;
+};
+
+
 
 	
 class TFileCaster : public TCaster, public SoyWorkerThread
@@ -23,8 +105,11 @@ protected:
 
 protected:
 	std::shared_ptr<TMediaEncoder>		mEncoder;
+	std::shared_ptr<TMultiplexerMp4>	mMulitplexer;
 	std::shared_ptr<TMediaPacketBuffer>	mFrameBuffer;
+
 	std::ofstream		mFileStream;
+	TStreamBuffer		mFileData;
 };
 
 
