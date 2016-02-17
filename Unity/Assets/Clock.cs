3fa45cebed2db45035ecd7014a59499bfe45cd2c
Graham Reeves <graham@grahamreeves.com>
2016-02-17 00:23:25 +0000

    Display clock in demo app to aid sync test

---
+++ Unity/Assets/Clock.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clock : MonoBehaviour {
+
+	public UnityEngine.UI.Text	mText;
+
+	void Update () {
+
+		if ( mText != null )
+			mText.text = "" + Time.time;
+	
+	}
+}
