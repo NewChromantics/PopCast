c9c6e620b2c94437c590375bfd1f311eecff4bbc
Graham Reeves <graham@grahamreeves.com>
2016-05-14 20:03:06 +0100

    Updated scripts from popmovie. Windows now builds to win32/x64 directories

--- CopyCSharpFiles.sh
+++ CopyCSharpFiles.sh
@@ -3,14 +3,22 @@
 echo "Copying c# to Unity dir"
 UNITY_PLUGINS_PATH="$SRCROOT/Unity/Assets/$PROJECT"
 mkdir -p $UNITY_PLUGINS_PATH
+
+# gr: using quotes with *.cs fails - file not found...
 cp $SRCROOT/src/*.cs $UNITY_PLUGINS_PATH
 if [ $? -ne 0 ]; then { echo "copy c# Failed." ; exit 1; } fi
 cp -r "$SRCROOT/src/Editor" $UNITY_PLUGINS_PATH
 if [ $? -ne 0 ]; then { echo "copy c# Failed." ; exit 1; } fi
 
-# replace GIT_REVISION with the git revision in the copied file
-
+# replace GIT_REVISION(the string) with the git revision in the copied file(GIT_REV, the variable)
+# can't seem to catch when this fails (ie, cant find git.exe)
 export GIT_REV=`git rev-parse --verify --short HEAD`
+if [ -z "${GIT_REV+1}"  ]; then
+{
+	export GIT_REV=GIT_NOT_FOUND
+} fi
+
+# this is the string we're going to match
 export GIT_REV_MATCH=GIT_REVISION
 
 
@@ -22,6 +30,7 @@ else
 {
 	echo "Replacing $GIT_REV_MATCH in C# with $GIT_REV..."
 	# streamedit -inplace <backupextension> -e command
+	# gr: using quotes with *.cs fails - file not found here too...
 	sed -i '' -e "s/$GIT_REV_MATCH/$GIT_REV/g" $UNITY_PLUGINS_PATH/*.cs
 	# gr: on windows, sed not known... error code still 0 :/
 	if [ $? -ne 0 ]; then
