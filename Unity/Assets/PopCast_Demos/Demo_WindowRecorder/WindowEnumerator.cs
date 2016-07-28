9c8b726767955a3b908fb09d08aba7b28464599e
Graham Reeves <graham@grahamreeves.com>
2016-07-28 22:20:37 +0100

    Added thumbnail gui object to setup, added creation of thumbnails

---
+++ Unity/Assets/PopCast_Demos/Demo_WindowRecorder/WindowEnumerator.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class WindowEnum 
+{
+	public RenderTexture			Texture; 
+	public string					Source;
+	public WindowSourceGuiThumbnail	ThumbnailGui;
+};
+
+
+
+public class WindowEnumerator : MonoBehaviour {
+
+	public WindowSourceGuiThumbnail	TemplateThumbnail;
+	public int						MaxWindows = 3;
+	private uint					EnumIndex = 0;
+	public List<WindowEnum>			Windows;
+	public string					WindowPrefix = "window:";
+	public string					WindowFilter;
+	public Material					InitThumbnailShader;
+	static Texture2D				ClearTextureCache;
+
+	void ClearTexture(RenderTexture Tex)
+	{
+		if ( InitThumbnailShader != null )
+		{
+			Graphics.Blit( Tex, InitThumbnailShader );
+			return;	
+		}
+
+		if (ClearTextureCache == null)
+		{ 
+			ClearTextureCache = new Texture2D(1,1);
+			ClearTextureCache.SetPixel( 0, 0, new Color(0,0,0) );
+		}
+
+		Graphics.Blit( Tex, ClearTextureCache );
+	}
+
+	void FindNewSource()
+	{ 
+		string SourceName = PopMovie.EnumSource(EnumIndex);
+
+		if ( SourceName == null )
+			return;
+
+		if (SourceName.StartsWith(WindowPrefix))
+		{ 
+			bool Filtered = (WindowFilter == null || WindowFilter.Length == 0) ? true : SourceName.Contains(WindowFilter);
+			if ( Filtered )
+			{
+				 AddNewSource(SourceName);
+			}
+		}
+
+		EnumIndex++;
+	}
+
+	void AddNewSource(string SourceName)
+	{ 
+		Debug.Log("New window " + SourceName );
+
+		if ( TemplateThumbnail == null )
+			return;
+
+		if ( Windows != null && Windows.Count >= MaxWindows )
+			return;
+
+		WindowEnum Enum = new WindowEnum();
+		Enum.Source = SourceName;
+		Enum.Texture = new RenderTexture(128,128,0);
+		ClearTexture( Enum.Texture );
+		Enum.ThumbnailGui = Instantiate( TemplateThumbnail );
+		Enum.ThumbnailGui.gameObject.SetActive( true );
+		Enum.ThumbnailGui.transform.SetParent( TemplateThumbnail.transform.parent );
+		Enum.ThumbnailGui.Init( Enum.Source, Enum.Texture );
+		if ( Windows == null )
+			Windows = new List<WindowEnum>();
+		Windows.Add( Enum );
+	}
+
+
+	void Update () {
+		FindNewSource();
+	}
+}
---
+++ Unity/Assets/PopCast_Demos/Demo_WindowRecorder/WindowSourceGuiThumbnail.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowSourceGuiThumbnail : MonoBehaviour {
+
+	public PopMovieParams	Params;
+	PopMovie		Movie;
+	Texture			ThumbnailTexture;
+	float			MovieTime = 0;
+
+	public void Init(string Filename,Texture _ThumbnailTexture)
+	{
+		ThumbnailTexture = _ThumbnailTexture;
+
+		var ThumbnailImage = GetComponent<UnityEngine.UI.RawImage>();
+		if (ThumbnailImage != null)
+		{ 
+			ThumbnailImage.texture = ThumbnailTexture;
+		}
+		
+		var Label = GetComponent<UnityEngine.UI.Text>();
+		if (Label != null)
+		{ 
+			Label.text = Filename;
+		}
+
+		try
+		{
+			Movie = new PopMovie(Filename, Params);
+		}
+		catch (System.Exception e)
+		{
+			Debug.LogWarning("Failed to create popmovie window capture: "  + Filename ); 
+		}
+
+	}
+
+	public void Update()
+	{
+		MovieTime += Time.deltaTime;
+		if (Movie != null && ThumbnailTexture != null)
+		{
+			Movie.SetTime( MovieTime );
+			Movie.UpdateTexture( ThumbnailTexture );
+		}
+
+	}
+
+
+}
